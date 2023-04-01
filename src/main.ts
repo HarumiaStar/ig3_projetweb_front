22791eb98cb9186ac8e46639637d4c916fc624d9
root <root@JetbrainsGateway.local>
2023-04-01 15:29:39 +0000

    init: Vuejs, Bulma, Oruga

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+import Oruga from '@oruga-ui/oruga-next'
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
+import '@oruga-ui/theme-bulma/dist/bulma.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(Oruga, bulmaConfig)
+
+app.mount('#app')
